0d6d93368a3e9a6e30cd69fd3e9af823607d558b
luke <lukeerosa@gmail.com>
2025-08-06 16:39:15 +0200

    added 3divs at screen1

--- js.js
+++ js.js
@@ -1,59 +1,43 @@
 const texts = document.querySelectorAll(".text");
+const texts1 = document.querySelectorAll(".text1");
 const btnL = document.querySelector(".btnL");
 const btnR = document.querySelector(".btnR");
 
 let indice = 1;
 
+function aggTasti (){
 
-/*function aggiornamentoDiv() {
-    texts.forEach((el, i) => {
-        // Rimuovo tutte le classi prima di aggiornarle
-        el.classList.remove("attivo", "sinistra", "destra");
+    texts.forEach(el=> el.classList.remove("attivo","sinistra","destra"))
 
-        if (i === indice) {
-            // Il div centrale visibile
-            el.classList.add("attivo");
-        } 
-         else if(texts[2].classList.contains("attivo")){
-            texts[0].classList.add("destra");
-        } else if (i < indice) {
-            // I div con indice più piccolo vanno a sinistra
-            el.classList.add("sinistra");
-        } else if(i>indice){
-            // I div con indice più grande vanno a destra
-            el.classList.add("destra");
-        } 
+    //div centrale
+    texts[indice].classList.add("attivo");
+    texts1[indice].classList.add("attivo");
 
-    });
-}*/
 
-function aggiornamentoDiv() {
-  texts.forEach(el => el.classList.remove("attivo", "sinistra", "destra"));
+    //calcolo dei div 
+    let len = texts.length
+    let sinistro = (indice - 1 + texts.length) % len;
+    let destro = (indice + 1) % len;
+    
+    texts[sinistro].classList.add("sinistra");
+    texts[destro].classList.add("destra");
+    
 
-  // Div centrale
-  texts[indice].classList.add("attivo");
-
-  // Calcola gli indici degli altri due
-  const len = texts.length;
-  const sinistra = (indice - 1 + len) % len;
-  const destra = (indice + 1) % len;
-
-  // Applica classi ai div a sinistra e a destra
-  texts[sinistra].classList.add("sinistra");
-  texts[destra].classList.add("destra");
+    
 }
+aggTasti();
 
-btnR.addEventListener("click",()=>{
-    indice = (indice + 1) % texts.length;
-        
-        aggiornamentoDiv();
+btnL.addEventListener("click", ()=>{
+    indice--
+    if(indice<0){
+        indice = texts.length-1;
     }
-
-)
-btnL.addEventListener("click",()=>{
-    indice = (indice - 1 + texts.length) % texts.length;
-        
-        aggiornamentoDiv();
+    aggTasti()
+})
+btnR.addEventListener("click", ()=>{
+    indice++
+    if(indice>texts.length -1){
+        indice = 0;
     }
- )
-aggiornamentoDiv();
+    aggTasti()
+})
