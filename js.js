d1061005a8482d9502ea90b710a8f8e5728f4064
luke <lukeerosa@gmail.com>
2025-08-06 15:49:13 +0200

    adde functions to make the transition to one div to another

--- js.js
+++ js.js
@@ -4,22 +4,56 @@ const btnR = document.querySelector(".btnR");
 
 let indice = 1;
 
-function aggiornamentoDiv(){
-    texts.forEach(texts => texts.classList.remove("attivo"));
-    texts[indice].classList.add("attivo");
+
+/*function aggiornamentoDiv() {
+    texts.forEach((el, i) => {
+        // Rimuovo tutte le classi prima di aggiornarle
+        el.classList.remove("attivo", "sinistra", "destra");
+
+        if (i === indice) {
+            // Il div centrale visibile
+            el.classList.add("attivo");
+        } 
+         else if(texts[2].classList.contains("attivo")){
+            texts[0].classList.add("destra");
+        } else if (i < indice) {
+            // I div con indice più piccolo vanno a sinistra
+            el.classList.add("sinistra");
+        } else if(i>indice){
+            // I div con indice più grande vanno a destra
+            el.classList.add("destra");
+        } 
+
+    });
+}*/
+
+function aggiornamentoDiv() {
+  texts.forEach(el => el.classList.remove("attivo", "sinistra", "destra"));
+
+  // Div centrale
+  texts[indice].classList.add("attivo");
+
+  // Calcola gli indici degli altri due
+  const len = texts.length;
+  const sinistra = (indice - 1 + len) % len;
+  const destra = (indice + 1) % len;
+
+  // Applica classi ai div a sinistra e a destra
+  texts[sinistra].classList.add("sinistra");
+  texts[destra].classList.add("destra");
 }
 
 btnR.addEventListener("click",()=>{
-    if(indice<texts.length-1){
-        indice++;
+    indice = (indice + 1) % texts.length;
+        
         aggiornamentoDiv();
     }
 
-})
+)
 btnL.addEventListener("click",()=>{
-    if(indice>0){
-        indice--;
+    indice = (indice - 1 + texts.length) % texts.length;
+        
         aggiornamentoDiv();
     }
-} )
+ )
 aggiornamentoDiv();
